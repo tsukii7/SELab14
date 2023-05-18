613f6f0e842c2a16105311a76745a296ab7d95a3
tsukii7 <1375926565@qq.com>
2023-05-18 12:07:27 +0800

    initialize

---
+++ SELab14/src/main/java/com/example/selab14/SeLab14Application.java
@@ -0,0 +1,13 @@
+package com.example.selab14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeLab14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeLab14Application.class, args);
+    }
+
+}
---
+++ SELab14/src/test/java/com/example/selab14/SeLab14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selab14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeLab14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
